596b5d30fd9627c086d5e4af291174cfa16c12c7
Vincent Loupmon <6957532+Vloupmon@users.noreply.github.com>
2021-10-31 16:58:07 +0100

    Moved plugins from .zshrc into slim.zsh

--- slim.zsh
+++ slim.zsh
@@ -20,6 +20,11 @@ source $slim_path/aliases.zsh
 source $slim_path/correction.zsh
 source $slim_path/stack.zsh
 source $slim_path/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $slim_path/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+source $slim_path/plugins/git/git.plugin.zsh
+source $slim_path/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $slim_path/plugins/ssh-agent/ssh-agent.plugin.zsh
+source $slim_path/plugins/colored-man-pages/colored-man-pages.plugin.zsh
 
 if command -v fasd >/dev/null 2>&1; then
   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
